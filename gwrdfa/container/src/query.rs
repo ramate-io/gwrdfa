f68913ccdf28e7149502f17eddb37053d6d48fea
Liam Makena Monninger <liam@ramate.io>
2026-02-17 16:07:19 +0000

    chore: simplify generic api for container query.

--- gwrdfa/container/src/query.rs
+++ gwrdfa/container/src/query.rs
@@ -1,21 +1,23 @@
 use crate::{ContainerEntity, ContainerEntityBuffer, ContainerGiving};
+use core::marker::PhantomData;
 use parabyzantine::buffer::{QueryPlanlike, Querylike};
 
 /// A query over a container.
-pub struct ContainerQuery<'a, T: Sized> {
+pub struct ContainerQuery<'a, T: ContainerGiving<'a, B> + Sized, B> {
 	buffer: &'a ContainerEntityBuffer<T>,
 	iter: std::collections::hash_map::Iter<'a, ContainerEntity, T>,
+	_phantom: PhantomData<B>,
 }
 
-impl<'a, T: Sized> ContainerQuery<'a, T> {
+impl<'a, T: ContainerGiving<'a, B> + Sized, B> ContainerQuery<'a, T, B> {
 	/// Creates a new query over a container.
 	pub fn new(buffer: &'a ContainerEntityBuffer<T>) -> Self {
-		Self { buffer, iter: buffer.iter() }
+		Self { buffer, iter: buffer.iter(), _phantom: PhantomData }
 	}
 }
 
 impl<'a, T: ContainerGiving<'a, B> + Sized, B>
-	Querylike<ContainerEntity, ContainerEntityBuffer<T>, B> for ContainerQuery<'a, T>
+	Querylike<ContainerEntity, ContainerEntityBuffer<T>, B> for ContainerQuery<'a, T, B>
 {
 	fn next(&mut self) -> Option<(ContainerEntity, B)> {
 		self.iter.next().map(|(entity, data)| (entity.clone(), data.as_item()))
@@ -30,50 +32,27 @@ impl<'a, T: ContainerGiving<'a, B> + Sized, B>
 #[derive(Debug, Clone, Copy)]
 pub struct AllContainerEntities;
 
-impl<'a, T: ContainerGiving<'a, B> + Sized, B>
-	QueryPlanlike<'a, ContainerEntity, ContainerEntityBuffer<T>, B, ContainerQuery<'a, T>>
+impl<'a, T: ContainerGiving<'a, B> + Sized, B: 'a>
+	QueryPlanlike<'a, ContainerEntity, ContainerEntityBuffer<T>, B, ContainerQuery<'a, T, B>>
 	for AllContainerEntities
 {
-	fn build(self, buffer: &'a ContainerEntityBuffer<T>) -> ContainerQuery<'a, T> {
+	fn build(self, buffer: &'a ContainerEntityBuffer<T>) -> ContainerQuery<'a, T, B> {
 		ContainerQuery::new(buffer)
 	}
 }
 
-pub struct ContainerMatchingQuery<'a, T: Sized> {
-	container_query: ContainerQuery<'a, T>,
-}
+#[cfg(test)]
+mod test {
+	use super::*;
+	use crate::container::test::TestContainer;
 
-impl<'a, T: Sized> ContainerMatchingQuery<'a, T> {
-	pub fn new(container_query: ContainerQuery<'a, T>) -> Self {
-		Self { container_query }
-	}
-}
-
-impl<'a, T: ContainerGiving<'a, Option<B>> + Sized, B>
-	Querylike<ContainerEntity, ContainerEntityBuffer<T>, B> for ContainerMatchingQuery<'a, T>
-{
-	fn next(&mut self) -> Option<(ContainerEntity, B)> {
-		// filter out None values
-		self.container_query
-			.next()
-			.and_then(|(entity, data)| data.map(|data| (entity, data)))
-	}
-
-	fn get(&self, entity: ContainerEntity) -> Option<B> {
-		// map the data to the bundle
-		self.container_query.get(entity).flatten()
-	}
-}
-
-/// The plan to query all matching container entities.
-#[derive(Debug, Clone, Copy)]
-pub struct AllMatchingContainerEntities;
-
-impl<'a, T: ContainerGiving<'a, Option<B>> + Sized, B>
-	QueryPlanlike<'a, ContainerEntity, ContainerEntityBuffer<T>, B, ContainerMatchingQuery<'a, T>>
-	for AllMatchingContainerEntities
-{
-	fn build(self, buffer: &'a ContainerEntityBuffer<T>) -> ContainerMatchingQuery<'a, T> {
-		ContainerMatchingQuery::new(ContainerQuery::new(buffer))
+	#[test]
+	fn test_all_container_entities() {
+		let mut buffer: ContainerEntityBuffer<TestContainer> = ContainerEntityBuffer::new();
+		let container = TestContainer::default();
+		let entity = buffer.insert_container(container);
+		let query_plan = AllContainerEntities;
+		let mut query = query_plan.build(&buffer);
+		assert_eq!(query.next(), Some((entity, 0 as i32)));
 	}
 }
