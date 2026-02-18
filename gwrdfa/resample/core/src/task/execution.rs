5bc9c9cc382d313e5c39904f818f56fad21e878e
Liam Makena Monninger <liam@ramate.io>
2026-02-18 13:50:30 +0000

    chore: implement resample tasker for noop.

--- gwrdfa/resample/core/src/task/execution.rs
+++ gwrdfa/resample/core/src/task/execution.rs
@@ -2,6 +2,7 @@ use super::{IndexTaskSubcommitteeAgreement, TaskSubcommittee};
 use parabyzantine::{
 	buffer::{Facts, Inferences},
 	task::{ParabyzantineTaskDataBinding, ParabyzantineTaskDataSpec},
+	NoOp,
 };
 
 /// A [ResampleTasker] is a trait that can compute a resample task.
@@ -45,4 +46,41 @@ pub trait ResampleTasker<
 			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskDraftBuffer,
 		>,
 	);
+}
+
+impl<
+		Index: Eq,
+		Sender: Eq,
+		Sub: TaskSubcommittee<Sender>,
+		SubAg: IndexTaskSubcommitteeAgreement<Index, Sender, Sub>,
+		Binding: ParabyzantineTaskDataBinding,
+	> ResampleTasker<Index, Sender, Sub, SubAg, Binding> for NoOp
+{
+	fn compute_resample_task(
+		&mut self,
+		_index: &SubAg,
+		_agreement_facts: &Facts<
+			<Binding::Spec as ParabyzantineTaskDataSpec>::AgreementEntity,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::AgreementBuffer,
+		>,
+		_transaction_facts: &Facts<
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TransactionEntity,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TransactionBuffer,
+		>,
+		_transaction_inferences: &mut Inferences<
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TransactionEntity,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TransactionBuffer,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TransactionDraftBuffer,
+		>,
+		_task_facts: &Facts<
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskEntity,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskBuffer,
+		>,
+		_task_inferences: &mut Inferences<
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskEntity,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskBuffer,
+			<Binding::Spec as ParabyzantineTaskDataSpec>::TaskDraftBuffer,
+		>,
+	) {
+	}
 }
