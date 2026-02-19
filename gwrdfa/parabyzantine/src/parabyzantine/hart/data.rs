d5ad1a2c6c9a48b4d821551d49851648d7760982
Liam Makena Monninger <liam@ramate.io>
2026-02-19 17:53:19 +0000

    chore: expand parabyzantine hart trait.

--- gwrdfa/parabyzantine/src/parabyzantine/hart/data.rs
+++ gwrdfa/parabyzantine/src/parabyzantine/hart/data.rs
@@ -3,6 +3,7 @@ pub mod as_message_in;
 pub mod as_message_out;
 pub mod as_task;
 
+use crate::act::Act;
 use crate::buffer::{facts::Facts, inferences::Inferences, Bufferlike, DraftBufferlike};
 use crate::{NoOp, NoOpData};
 
@@ -107,6 +108,20 @@ pub trait ParabyzantineData<Spec: ParabyzantineDataSpec>: Sized {
 			message_inferences: self.parabyzantine_message_draft_buffer().into(),
 		}
 	}
+
+	/// Commits the inferences for the parabyzantine world.
+	fn commit_parabyzantine_hart(&mut self, inferences: ParabyzantineHartInferences<Spec>) {
+		self.parabyzantine_certificate_buffer_mut()
+			.commit_inferences(inferences.certificate_inferences);
+		self.parabyzantine_agreement_buffer_mut()
+			.commit_inferences(inferences.agreement_inferences);
+		self.parabyzantine_transaction_buffer_mut()
+			.commit_inferences(inferences.transaction_inferences);
+		self.parabyzantine_task_buffer_mut()
+			.commit_inferences(inferences.task_inferences);
+		self.parabyzantine_message_buffer_mut()
+			.commit_inferences(inferences.message_inferences);
+	}
 }
 
 pub struct ParabyzantineWorld<'a, Spec: ParabyzantineDataSpec> {
@@ -140,15 +155,70 @@ pub struct ParabyzantineWorld<'a, Spec: ParabyzantineDataSpec> {
 		Inferences<Spec::MessageEntity, Spec::MessageBuffer, Spec::MessageDraftBuffer>,
 }
 
+pub struct ParabyzantineHartInferences<Spec: ParabyzantineDataSpec> {
+	pub certificate_inferences:
+		Inferences<Spec::CertificateEntity, Spec::CertificateBuffer, Spec::CertificateDraftBuffer>,
+	pub agreement_inferences:
+		Inferences<Spec::AgreementEntity, Spec::AgreementBuffer, Spec::AgreementDraftBuffer>,
+	pub transaction_inferences:
+		Inferences<Spec::TransactionEntity, Spec::TransactionBuffer, Spec::TransactionDraftBuffer>,
+	pub task_inferences: Inferences<Spec::TaskEntity, Spec::TaskBuffer, Spec::TaskDraftBuffer>,
+	pub message_inferences:
+		Inferences<Spec::MessageEntity, Spec::MessageBuffer, Spec::MessageDraftBuffer>,
+}
+
+impl<'a, Spec: ParabyzantineDataSpec> From<ParabyzantineWorld<'a, Spec>>
+	for ParabyzantineHartInferences<Spec>
+{
+	fn from(world: ParabyzantineWorld<'a, Spec>) -> Self {
+		ParabyzantineHartInferences {
+			certificate_inferences: world.certificate_inferences,
+			agreement_inferences: world.agreement_inferences,
+			transaction_inferences: world.transaction_inferences,
+			task_inferences: world.task_inferences,
+			message_inferences: world.message_inferences,
+		}
+	}
+}
+
 /// A [ParabyzantineHart] trait describes operations on the parabyzantine hart.
 pub trait ParabyzantineHart: Sized {
 	type Binding: ParabyzantineDataBinding;
+
+	/// Borrows the [ParabyzantineWorld] for the parabyzantine hart.
+	fn parabyzantine_hart_world<'a>(
+		&mut self,
+		data: &'a mut <Self::Binding as ParabyzantineDataBinding>::Data,
+	) -> ParabyzantineWorld<'a, <Self::Binding as ParabyzantineDataBinding>::Spec> {
+		data.parabyzantine_world()
+	}
 
 	/// Compute the parabyzantine hart.
 	fn update_parabyzantine_hart(
 		&mut self,
 		data: &mut ParabyzantineWorld<<Self::Binding as ParabyzantineDataBinding>::Spec>,
 	);
+
+	/// Commits the inferences for the parabyzantine hart.
+	fn commit_parabyzantine_hart(
+		&mut self,
+		hart_inferences: ParabyzantineHartInferences<
+			<Self::Binding as ParabyzantineDataBinding>::Spec,
+		>,
+		data: &mut <Self::Binding as ParabyzantineDataBinding>::Data,
+	) {
+		data.commit_parabyzantine_hart(hart_inferences);
+	}
+}
+
+impl<Binding: ParabyzantineDataBinding, HartHandler: ParabyzantineHart<Binding = Binding>>
+	Act<Hart, Binding::Data> for HartHandler
+{
+	fn act(&mut self, _action: Hart, data: &mut Binding::Data) {
+		let mut world = self.parabyzantine_hart_world(data);
+		self.update_parabyzantine_hart(&mut world);
+		self.commit_parabyzantine_hart(world.into(), data);
+	}
 }
 
 /// A [ParabyzantineDataBinding] is a binding for the [Parabyzantine] protocol.
