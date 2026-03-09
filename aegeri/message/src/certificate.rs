e506709a8ee9273eb9ea1a91fa2c23f9b65caf35
Liam Makena Monninger <liam@ramate.io>
2026-03-09 15:45:22 +0100

    chore: clean up types to match design.

--- aegeri/message/src/certificate.rs
+++ aegeri/message/src/certificate.rs
@@ -1,4 +1,4 @@
-use super::Id;
+use super::{Id, PublicKey, Transaction};
 use serde::{Deserialize, Serialize};
 
 /// The index of a block in the system.
@@ -7,19 +7,28 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Index(u64);
 
-/// The block itself which is a set of transaction ids.
+/// The block itself which is a set of transactions.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Block(Vec<Id>);
+pub struct Block(Vec<Transaction>);
+
+/// The header of the block which references the transactions in the block.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BlockHeader(Vec<Id>);
 
 /// The state root produced by execution of the block.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct StateRoot(Vec<u8>);
 
+/// The JoinSet for a block.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct JoinSet(Vec<PublicKey>);
+
 /// The unified value of a certificate.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Value {
-	block: Block,
+	block: BlockHeader,
 	state_root: StateRoot,
+	join_set: JoinSet,
 }
 
 /// The certificate for a block.
--- aegeri/process/src/lib.rs
+++ aegeri/process/src/lib.rs
@@ -4,4 +4,3 @@ pub use aegeri_message;
 pub use gossamer;
 pub use gwrdfa_resample;
 pub use parabyzantine;
-pub use task::{AegeriTaskFlow, TaskFlowError, TaskFlowResult};
