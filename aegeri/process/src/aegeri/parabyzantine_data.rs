0b6e524b30c72e4ad735bb9fd872b6c8576a57fe
Liam Makena Monninger <liam@ramate.io>
2026-03-12 17:19:06 +0100

    feat: complete buffer model.

--- aegeri/process/src/aegeri/parabyzantine_data.rs
+++ aegeri/process/src/aegeri/parabyzantine_data.rs
@@ -1,17 +1,21 @@
+use crate::buffers::{
+	TaskContainer, TaskDeltasContainer, TransactionContainer, TransactionDeltasContainer,
+};
 use aegeri_message::{AegeriSubcommittee, Index, Proposal, UnifiedMessage};
 use gossamer::container::{GossamerContainer, GossamerDeltasContainer};
 use gwrdfa_container::{ContainerEntity, ContainerEntityBuffer, ContainerEntityDraftBuffer};
 use gwrdfa_resample::agreement::std::container::{
 	AgreementContainer, AgreementDelta, CertificateContainer, CertificateDelta,
 };
-use parabyzantine::{NoOp, NoOpData, ParabyzantineData};
+use parabyzantine::ParabyzantineData;
 
 pub struct AegeriParabyzantineData {
 	pub messages: ContainerEntityBuffer<GossamerContainer<UnifiedMessage>>,
 	pub certificates:
 		ContainerEntityBuffer<CertificateContainer<Index, Proposal, AegeriSubcommittee>>,
+	pub transactions: ContainerEntityBuffer<TransactionContainer>,
 	pub agreements: ContainerEntityBuffer<AgreementContainer<Index, Proposal, AegeriSubcommittee>>,
-	pub noop: NoOpData,
+	pub tasks: ContainerEntityBuffer<TaskContainer>,
 }
 
 impl AegeriParabyzantineData {
@@ -20,7 +24,8 @@ impl AegeriParabyzantineData {
 			messages: ContainerEntityBuffer::new(),
 			certificates: ContainerEntityBuffer::new(),
 			agreements: ContainerEntityBuffer::new(),
-			noop: NoOpData::new(),
+			transactions: ContainerEntityBuffer::new(),
+			tasks: ContainerEntityBuffer::new(),
 		}
 	}
 }
@@ -31,29 +36,29 @@ impl ParabyzantineData for AegeriParabyzantineData {
 	type MessageBuffer = ContainerEntityBuffer<GossamerContainer<UnifiedMessage>>;
 	type MessageDraftBuffer = ContainerEntityDraftBuffer<GossamerDeltasContainer<UnifiedMessage>>;
 
-	// Certificates and agreements are stored in the same container.
+	// Certificates are stoed in a ResampleAgreement container
 	type CertificateEntity = ContainerEntity;
 	type CertificateBuffer =
 		ContainerEntityBuffer<CertificateContainer<Index, Proposal, AegeriSubcommittee>>;
 	type CertificateDraftBuffer =
 		ContainerEntityDraftBuffer<CertificateDelta<Index, Proposal, AegeriSubcommittee>>;
 
-	// Certificates and agreements are stored in the same container.
+	// Transactions are stored in a TransactionContainer
+	type TransactionEntity = ContainerEntity;
+	type TransactionBuffer = ContainerEntityBuffer<TransactionContainer>;
+	type TransactionDraftBuffer = ContainerEntityDraftBuffer<TransactionDeltasContainer>;
+
+	// Agreements are stored in a ResampleAgreement container
 	type AgreementEntity = ContainerEntity;
 	type AgreementBuffer =
 		ContainerEntityBuffer<AgreementContainer<Index, Proposal, AegeriSubcommittee>>;
 	type AgreementDraftBuffer =
 		ContainerEntityDraftBuffer<AgreementDelta<Index, Proposal, AegeriSubcommittee>>;
 
-	// Transactions are not used in Aegeri.
-	type TransactionEntity = NoOp;
-	type TransactionBuffer = NoOp;
-	type TransactionDraftBuffer = NoOp;
-
-	// Tasks are not used in Aegeri.
-	type TaskEntity = NoOp;
-	type TaskBuffer = NoOp;
-	type TaskDraftBuffer = NoOp;
+	// Tasks are stored in a TaskContainer
+	type TaskEntity = ContainerEntity;
+	type TaskBuffer = ContainerEntityBuffer<TaskContainer>;
+	type TaskDraftBuffer = ContainerEntityDraftBuffer<TaskDeltasContainer>;
 
 	fn parabyzantine_message_buffer(&self) -> &Self::MessageBuffer {
 		&self.messages
@@ -79,6 +84,18 @@ impl ParabyzantineData for AegeriParabyzantineData {
 		Self::CertificateDraftBuffer::new()
 	}
 
+	fn parabyzantine_transaction_buffer(&self) -> &Self::TransactionBuffer {
+		&self.transactions
+	}
+
+	fn parabyzantine_transaction_buffer_mut(&mut self) -> &mut Self::TransactionBuffer {
+		&mut self.transactions
+	}
+
+	fn parabyzantine_transaction_draft_buffer(&self) -> Self::TransactionDraftBuffer {
+		Self::TransactionDraftBuffer::new()
+	}
+
 	fn parabyzantine_agreement_buffer(&self) -> &Self::AgreementBuffer {
 		&self.agreements
 	}
@@ -91,27 +108,15 @@ impl ParabyzantineData for AegeriParabyzantineData {
 		Self::AgreementDraftBuffer::new()
 	}
 
-	fn parabyzantine_transaction_buffer(&self) -> &Self::TransactionBuffer {
-		&self.noop.no_op
-	}
-
-	fn parabyzantine_transaction_buffer_mut(&mut self) -> &mut Self::TransactionBuffer {
-		&mut self.noop.no_op
-	}
-
-	fn parabyzantine_transaction_draft_buffer(&self) -> Self::TransactionDraftBuffer {
-		Self::TransactionDraftBuffer::default()
-	}
-
 	fn parabyzantine_task_buffer(&self) -> &Self::TaskBuffer {
-		&self.noop.no_op
+		&self.tasks
 	}
 
 	fn parabyzantine_task_buffer_mut(&mut self) -> &mut Self::TaskBuffer {
-		&mut self.noop.no_op
+		&mut self.tasks
 	}
 
 	fn parabyzantine_task_draft_buffer(&self) -> Self::TaskDraftBuffer {
-		Self::TaskDraftBuffer::default()
+		Self::TaskDraftBuffer::new()
 	}
 }
--- aegeri/process/src/buffers/transaction.rs
+++ aegeri/process/src/buffers/transaction.rs
@@ -1,2 +1,4 @@
 pub mod container;
+pub use container::TransactionContainer;
 pub mod delta_container;
+pub use delta_container::TransactionDeltasContainer;
