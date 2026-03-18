581ccf3a5a24152370c4f812f64637484d7c355d
Liam Makena Monninger <liam@ramate.io>
2026-03-18 12:33:28 +0100

    debug: waiting for all 7 before starting seems to be a safer bet because when you're running a lot of these p2p tasks on a smaller device it seems network partitions on common. If you have all 7 online, then you of course have a lower chance of a partition.

--- aegeri/full-client/src/tests/resource_acquiring.rs
+++ aegeri/full-client/src/tests/resource_acquiring.rs
@@ -59,10 +59,10 @@ async fn test_bootstrap_non_participant_leave_reaches_transition() -> Result<(),
 	});
 
 	let test_result = async {
-		// Waiting for 5 peers instead of 1 should make this wait deeper into the
+		// Waiting for 7 peers instead of 5 or 1 should make this wait deeper into the
 		// network startup before attempting to send transactions.
 		let (mut client, _listen_addr) =
-			FullClient::bootstrap_non_participant(topic, 5, bootstrap_peers).await?;
+			FullClient::bootstrap_non_participant(topic, 7, bootstrap_peers).await?;
 		let leave = leave_message(99, b"resource-acquiring-leave")?;
 		let index = client
 			.send_and_wait_for_transition(leave, Duration::from_secs(60))
